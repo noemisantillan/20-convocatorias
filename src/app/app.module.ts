686d9fcfade5fa0fee55281f84ef610650cb8291
Raulyoyo <raul.dmai@gmail.com>
2021-12-01 14:04:10 -0600

    v1.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { ConvocatoriaunoComponent } from './convocatoriauno/convocatoriauno.comp
 import { AppRoutingModule } from './app-routing.module';
 import { ConvocatoridosComponent } from './convocatoridos/convocatoridos.component';
 import { BraileComponent } from './braile/braile.component';
+import { ScrollTopComponent } from './componentes/scroll-top/scroll-top.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { BraileComponent } from './braile/braile.component';
     FooterComponent,
     ConvocatoriaunoComponent,
     ConvocatoridosComponent,
-    BraileComponent
+    BraileComponent,
+    ScrollTopComponent
   ],
   imports: [
     BrowserModule,
